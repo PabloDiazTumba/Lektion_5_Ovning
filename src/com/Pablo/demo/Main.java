f59d74072f4ba447b500af8c73c8dd782cfd3f1c
PabloDiazTumba <pablodiaz147@gmail.com>
2023-09-26 19:22:02 +0200

    scanner practice

---
+++ src/com/Pablo/demo/Main.java
@@ -0,0 +1,42 @@
+package com.Pablo.demo;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean isPlaying = true;
+
+        do {
+            int result = scannerNumber();
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("Looping...");
+            }
+            if (result > 1000) {
+                isPlaying = false;
+            }
+
+        } while (isPlaying);
+
+    }
+
+    public static int scannerNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+
+        return sc.nextInt();
+    }
+
+    public static String scannerText() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert some random sentence");
+        String input = sc.nextLine();
+
+        return input;
+    }
+}

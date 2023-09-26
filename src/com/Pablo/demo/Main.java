44258329eb273ac8a7003d1b351399073b830368
PabloDiazTumba <pablodiaz147@gmail.com>
2023-09-26 20:53:52 +0200

    New code

--- src/com/Pablo/demo/Main.java
+++ src/com/Pablo/demo/Main.java
@@ -1,40 +1,58 @@
 package com.Pablo.demo;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+        String playerName = sc.nextLine();
+        System.out.println("Welcome player #1: " + playerName);
+
+        int x;
+        while (true) {
+            System.out.println("Please enter a number (X):");
+
+            if (sc.hasNextInt()) {
+                x = sc.nextInt();
+                break;
+            } else {
+                System.out.println("That's not a number mate! Please enter a number.");
+                sc.next();
+            }
+        }
+
+        for (int i = 0; i < x; i++) {
+            System.out.println(i);
+            System.out.println("Looping...");
+        }
+
         boolean isPlaying = true;
 
         do {
-            int result = scannerNumber();
+            String userInput = scannerText();
 
-            for (int i = 0; i < result; i++) {
-                System.out.println(i);
-                System.out.println("Looping...");
-            }
-            if (result > 1000) {
-                isPlaying = false;
+            switch (userInput) {
+                case "Stop":
+                    isPlaying = false;
+                    break;
+                case "1":
+                    System.out.println("Throwing!");
+                    break;
+                default:
+                    System.out.println("Wrong input");
             }
 
         } while (isPlaying);
 
     }
 
-    public static int scannerNumber() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Please insert a number");
-
-        return sc.nextInt();
-    }
-
     public static String scannerText() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Insert some random sentence");
+        System.out.println("Please press 1, or write 'Stop' to stop playing ");
         String input = sc.nextLine();
 
         return input;
